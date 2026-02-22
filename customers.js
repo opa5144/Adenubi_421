4fdf0e76810ddd82ab04f6acf9894527ab161d3c
opa5144 <opa5144@psu.edu>
2026-02-22 18:14:45 -0500

    Updated customers.js
    Updated to support swagger configuration

--- customers.js
+++ customers.js
@@ -42,4 +42,144 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+// routes/customers.js
+
+/**
+* @swagger
+* /customers:
+*   get:
+*     tags:
+*       - Customers
+*     summary: Retrieve a list of customers
+*     responses:
+*       200:
+*         description: Successfully retrieved a list of customers
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*                 properties:
+*                   _id:
+*                     type: string
+*                   name:
+*                     type: string
+*                   phone:
+*                     type: string
+*                   email:
+*                     type: string
+*   post:
+*     tags:
+*       - Customers
+*     summary: Create a new customer
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             required:
+*               - name
+*               - phone
+*               - email
+*             properties:
+*               name:
+*                 type: string
+*                 example: John Numbers
+*               phone:
+*                 type: string
+*                 example: 555-555-5555
+*               email:
+*                 type: string
+*                 example: jnum@gmail.com
+*     responses:
+*       201:
+*         description: Customer successfully created
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 _id:
+*                   type: string
+*                 name:
+*                   type: string
+*                 phone:
+*                   type: string
+*                 email:
+*                   type: string
+*                 message:
+*                   type: string
+*                   description: Processing message
+* /customers/{id}:
+*   patch:
+*     tags:
+*       - Customers
+*     summary: Update a customer by ID
+*     parameters:
+*       - in: path
+*         name: id
+*         required: true
+*         schema:
+*           type: string
+*         description: Customer ID
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               name:
+*                 type: string
+*               phone:
+*                 type: string
+*               email:
+*                 type: string
+*     responses:
+*       200:
+*         description: Customer successfully updated
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 _id:
+*                   type: string
+*                 name:
+*                   type: string
+*                 phone:
+*                   type: string
+*                 email:
+*                   type: string
+*       400:
+*         description: Invalid request
+*   delete:
+*     tags:
+*       - Customers
+*     summary: Delete a customer by ID
+*     parameters:
+*       - in: path
+*         name: id
+*         required: true
+*         schema:
+*           type: string
+*         description: Customer ID
+*     responses:
+*       200:
+*         description: Customer successfully deleted
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 message:
+*                   type: string
+*       400:
+*         description: Invalid request
+*/
+   
+module.exports = router;
+
 module.exports = router;
